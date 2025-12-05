88e37dc5f9b31e957c27f47a007dc763aebb06cd
2006-sk <shresthkarnani@gmail.com>
2025-12-04 23:01:49 -0800

    Implementation of the food class

--- src/snake/Food.java
+++ src/snake/Food.java
@@ -1,6 +1,22 @@
 package snake;
 
-public class Food {
-    // Food implementation
-}
+import java.util.Random;
 
+public class Food {
+
+    private int x;
+    private int y;
+    private Random random = new Random();
+
+    public Food(int maxX, int maxY) {
+        spawn(maxX, maxY);
+    }
+
+    public void spawn(int maxX, int maxY) {
+        x = random.nextInt(maxX);
+        y = random.nextInt(maxY);
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+}
