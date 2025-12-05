8def09a0d7c1c86a8cf8978a2e10b0ca67689753
2006-sk <shresthkarnani@gmail.com>
2025-12-04 22:53:22 -0800

    snakesegment.java

--- src/snake/SnakeSegment.java
+++ src/snake/SnakeSegment.java
@@ -1,6 +1,22 @@
 package snake;
 
 public class SnakeSegment {
-    // SnakeSegment implementation
-}
 
+    private int x;
+    private int y;
+
+    public SnakeSegment(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // getters
+    public int getX() { return x; }
+    public int getY() { return y; }
+
+    // update position
+    public void setPosition(int newX, int newY) {
+        this.x = newX;
+        this.y = newY;
+    }
+}
