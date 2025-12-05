75ac9d6c644ed951aa07bd6168ec7a67a58dc776
2006-sk <shresthkarnani@gmail.com>
2025-12-04 22:56:14 -0800

    snake.java first addition

--- src/snake/Snake.java
+++ src/snake/Snake.java
@@ -1,6 +1,40 @@
 package snake;
 
-public class Snake {
-    // Snake implementation
-}
+import java.util.ArrayList;
 
+public class Snake {
+
+    private ArrayList<SnakeSegment> segments = new ArrayList<>();
+    private Direction currentDirection;
+
+    public enum Direction {
+        UP, DOWN, LEFT, RIGHT
+    }
+
+    public Snake(int startX, int startY) {
+        
+        segments.add(new SnakeSegment(startX, startY));
+        currentDirection = Direction.RIGHT;
+    }
+
+    public ArrayList<SnakeSegment> getSegments() {
+        return segments;
+    }
+
+    public Direction getDirection() {
+        return currentDirection;
+    }
+
+    public void setDirection(Direction dir) {
+        this.currentDirection = dir;
+    }
+
+    public void move() {
+        
+    }
+
+    public void grow() {
+        SnakeSegment tail = segments.get(segments.size() - 1);
+        segments.add(new SnakeSegment(tail.getX(), tail.getY()));
+    }
+}
