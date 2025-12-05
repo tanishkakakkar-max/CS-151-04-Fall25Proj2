4b61a3b23b0e6e55b293a34b0f78d46440b54c63
2006-sk <shresthkarnani@gmail.com>
2025-12-04 23:18:43 -0800

    First addition to the SnakeGame.java file

--- src/snake/SnakeGame.java
+++ src/snake/SnakeGame.java
@@ -3,3 +3,33 @@ package snake;
 import javafx.animation.AnimationTimer;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+
+public class SnakeGame {
+
+    private Snake snake;
+    private Food food;
+    private GameBoard board;
+    private boolean paused = false;
+    private AnimationTimer timer;
+    private int score = 0;
+
+    public Scene createGameScene() {
+
+        board = new GameBoard(25, 25, 20);
+        snake = new Snake(12, 12);
+        food = new Food(board.getCols(), board.getRows());
+
+        StackPane root = new StackPane();
+        root.getChildren().add(board.getCanvas());
+
+        Scene scene = new Scene(root);
+
+        scene.setOnKeyPressed(e -> {
+            switch (e.getCode()) {
+                case UP -> snake.setDirection(Snake.Direction.UP);
+                case DOWN -> snake.setDirection(Snake.Direction.DOWN);
+                case LEFT -> snake.setDirection(Snake.Direction.LEFT);
+                case RIGHT -> snake.setDirection(Snake.Direction.RIGHT);
+                case ESCAPE -> togglePause(root);
+            }
+        });
