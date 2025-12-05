ba504272bbdbc7ad165a78a47faabd427cc01625
2006-sk <shresthkarnani@gmail.com>
2025-12-04 23:12:35 -0800

    Implementing the overlay that appears when ESC is pressed, freeing the game

--- src/snake/PauseOverlay.java
+++ src/snake/PauseOverlay.java
@@ -1,6 +1,20 @@
 package snake;
 
-public class PauseOverlay {
-    // PauseOverlay implementation
-}
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
+
+public class PauseOverlay extends StackPane {
+
+    public PauseOverlay() {
+        setStyle("-fx-background-color: rgba(0,0,0,0.6);");
+
+        Text pausedText = new Text("PAUSED");
+        pausedText.setFont(Font.font(40));
+        pausedText.setFill(Color.WHITE);
+
+        getChildren().add(pausedText);
+    }
+}
