7b50913a042c902386b96afcc9c43977ff132a79
2006-sk <shresthkarnani@gmail.com>
2025-12-04 23:34:56 -0800

    Add SnakeConstants class for board and timing configuration

---
+++ src/snake/SnakeConstants.java
@@ -0,0 +1,14 @@
+package snake;
+
+public class SnakeConstants {
+
+    public static final int BOARD_ROWS = 25;
+
+    public static final int BOARD_COLS = 25;
+
+    public static final int CELL_SIZE = 20;
+
+    public static final long FRAME_DELAY = 150_000_000L;  // speed tweak
+
+}
+
