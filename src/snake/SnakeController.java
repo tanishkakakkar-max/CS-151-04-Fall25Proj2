adebd5e964c87f4433f32e876d1b6d812d15086c
2006-sk <shresthkarnani@gmail.com>
2025-12-04 23:26:34 -0800

    Simple controller wrapper for launching the SnakeGame scene.

--- src/snake/SnakeController.java
+++ src/snake/SnakeController.java
@@ -1,3 +1,15 @@
 package snake;
 
 import javafx.scene.Scene;
+public class SnakeController {
+
+    private SnakeGame snakeGame;
+
+    public SnakeController() {
+        snakeGame = new SnakeGame();
+    }
+
+    public Scene getGameScene() {
+        return snakeGame.createGameScene();
+    }
+}
