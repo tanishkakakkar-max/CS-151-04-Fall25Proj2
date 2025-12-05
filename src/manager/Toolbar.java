06d5877870c1d0b83b60260334819bbb246d971f
2006-sk <shresthkarnani@gmail.com>
2025-12-04 23:31:37 -0800

    Contains a Main Menu button and can be extended later.

--- src/manager/Toolbar.java
+++ src/manager/Toolbar.java
@@ -3,3 +3,20 @@ package manager;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+
+public class Toolbar extends HBox {
+
+    private Button mainMenuButton;
+
+    public Toolbar(Runnable goToMainMenu) {
+
+        mainMenuButton = new Button("Main Menu");
+        mainMenuButton.setOnAction(e -> goToMainMenu.run());
+
+        setSpacing(20);
+        setAlignment(Pos.CENTER_LEFT);
+        getChildren().add(mainMenuButton);
+
+        setStyle("-fx-background-color: #333; -fx-padding: 10;");
+    }
+}
